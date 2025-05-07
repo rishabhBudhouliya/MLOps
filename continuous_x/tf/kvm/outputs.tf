abe998f6068e6e1827dd646d6262c38610f1db69
Riya <riyap5033@gmail.com>
2025-05-06 20:30:01 -0400

    Terraform and ansible configuration

---
+++ continuous_x/tf/kvm/outputs.tf
@@ -0,0 +1,4 @@
+output "floating_ip_out" {
+  description = "Floating IP assigned to node1"
+  value       = openstack_networking_floatingip_v2.floating_ip.address
+}
---
+++ continuous_x/tf/kvm/variables.tf
@@ -0,0 +1,14 @@
+variable "key" {
+  description = "Name of key pair"
+  type        = string
+  default     = "id_rsa_chameleon"
+}
+
+variable "nodes" {
+  type = map(string)
+  default = {
+    "node1" = "192.168.1.11"
+    "node2" = "192.168.1.12"
+    "node3" = "192.168.1.13"
+  }
+}
